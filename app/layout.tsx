3d1c5e0305f4b1cec1dbbe1fd9a2501254c8b45c
NitishYadav46 <nitishkumaryadav526@gmail.com>
2026-08-18 19:58:08 +0530

    Add background color to body in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import './globals.css'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-[#07090e]">{children}</body>
     </html>
   );
 }
